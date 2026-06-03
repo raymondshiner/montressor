6647c5135a56268360e7e1979e7feb7c6578f595
sirlexicon <raymondshiner@gmail.com>
2026-06-02 17:08:32 -0700

    symlink-ify configs; commit hyprland.conf walker rebind, config.fish drift, add hyprpaper.conf

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# Dark mode — tells Qt and GTK apps to use dark theme
+set -gx GTK_THEME adw-gtk3-dark
+set -gx QT_QPA_PLATFORMTHEME qt5ct
+set -gx QT_STYLE_OVERRIDE adwaita-dark
+set -gx COLORTERM truecolor
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
